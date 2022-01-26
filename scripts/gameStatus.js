dcce6df90979d46fa34777f7acc413f2561142f4
KevenyLima <92321571+KevenyLima@users.noreply.github.com>
2022-01-26 19:38:23 -0300

    rearrangement of code

---
+++ scripts/gameStatus.js
@@ -0,0 +1,13 @@
+const player1_score=document.getElementById('Player1_score')
+const player2_score=document.getElementById('Player2_score')
+
+const game ={
+    players:{
+        'player1':{x:1,y:1,pontos:0},
+        'player2':{x:9,y:5,pontos:0},
+    },
+    fruits:{
+        'fruit1':{x:3,y:1},
+        'fruit2':{x:4,y:7}
+    }
+}
